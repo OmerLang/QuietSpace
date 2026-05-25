98cc03cc2c0a99839227c02acfea3f752c4bc8d4
Langer6699 <langer6699@gmail.com>
2026-05-25 21:34:14 +0300

    theme button styling

--- src/components/ThemeToggle/ThemeToggle.js
+++ src/components/ThemeToggle/ThemeToggle.js
@@ -21,15 +21,21 @@ export const ThemeToggle = () => {
 
   return (
     <button className={styles.button} onClick={toggleTheme}>
-      {theme === "light" ? (
-        <svg viewBox="0 0 24 24">
-          <path
-            className={styles.moon}
-            d="M12 22C17.5228 22 22 17.5228 22 12C22 11.5373 21.3065 11.4608 21.0672 11.8568C19.9289 13.7406 17.8615 15 15.5 15C11.9101 15 9 12.0899 9 8.5C9 6.13845 10.2594 4.07105 12.1432 2.93276C12.5392 2.69347 12.4627 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
-          />
+      <div
+        className={`${styles.container} ${theme === "light" ? styles.showMoon : styles.showSun}`}
+      >
+        <svg
+          className={`${styles.moon} ${theme === "light" ? styles.show : ""}`}
+          viewBox="0 0 24 24"
+        >
+          <path d="M12 22C17.5228 22 22 17.5228 22 12C22 11.5373 21.3065 11.4608 21.0672 11.8568C19.9289 13.7406 17.8615 15 15.5 15C11.9101 15 9 12.0899 9 8.5C9 6.13845 10.2594 4.07105 12.1432 2.93276C12.5392 2.69347 12.4627 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" />
         </svg>
-      ) : (
-        <svg d="_x34_" viewBox="0 0 512 512" fill="#ecc92a">
+        <svg
+          className={`${styles.sun} ${theme === "dark" ? styles.show : ""}`}
+          d="_x34_"
+          viewBox="0 0 512 512"
+          fill="#ecc92a"
+        >
           <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
           <g
             id="SVGRepo_tracerCarrier"
@@ -92,7 +98,7 @@ export const ThemeToggle = () => {
             </g>{" "}
           </g>
         </svg>
-      )}
+      </div>
     </button>
   );
 };
